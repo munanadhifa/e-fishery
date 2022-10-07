7256d171c5c272d15950e6272b79640f61cf5a18
Muna Nadhifa Ridwan <mnadhifa@prixa.ai>
2022-10-07 09:41:31 +0700

    add filter

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -4,6 +4,19 @@ import "./style.scss";
 
 export default function Modal() {
   const [showModal, setShowModal] = useState(false);
+  const [area, setArea] = useState([]);
+
+  const fetchAreaData = () => {
+    fetch(
+      "https://stein.efishery.com/v1/storages/5e1edf521073e315924ceab4/option_area"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setArea(data);
+      });
+  };
   return (
     <>
       <div className="flex items-center justify-center">
@@ -33,46 +46,42 @@ export default function Modal() {
                     onClick={() => setShowModal(false)}
                   />
                 </div>
-                <div className="mt-3 sm:flex">
-                  <div className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-6 h-6 text-red-600"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                        clipRule="evenodd"
+                <fieldset className="space-y-5">
+                  <div className="relative flex items-start">
+                    <div className="flex h-5 items-center">
+                      <input
+                        id="comments"
+                        aria-describedby="comments-description"
+                        name="comments"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                       />
-                    </svg>
-                  </div>
-                  <div className="mt-2 text-center sm:ml-4 sm:text-left">
-                    <h4 className="text-lg font-medium text-gray-800">
-                      Delete account ?
-                    </h4>
-                    <p className="mt-2 text-[15px] leading-relaxed text-gray-500">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
-                    </p>
-                    <div className="items-center gap-2 mt-3 sm:flex">
-                      <button
-                        className="w-full mt-2 p-2.5 flex-1 text-white bg-red-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
-                        onClick={() => setShowModal(false)}
-                      >
-                        Delete
-                      </button>
-                      <button
-                        className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
-                        onClick={() => setShowModal(false)}
-                      >
-                        Cancel
-                      </button>
+                    </div>
+                    <div className="ml-3 text-sm">
+                      {area.map((user) => (
+                        <label
+                          htmlFor="comments"
+                          className="font-medium text-gray-700"
+                          key={user.id}
+                          value={user.id}
+                        >
+                          {user.province}
+                        </label>
+                      ))}
                     </div>
                   </div>
-                </div>
+                  <div className="relative flex items-start">
+                    <div className="flex h-5 items-center">
+                      <input
+                        id="candidates"
+                        aria-describedby="candidates-description"
+                        name="candidates"
+                        type="checkbox"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      />
+                    </div>
+                  </div>
+                </fieldset>
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import Table from "./Components/Table";
-// import Test from "./Components/Test";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Table />
-    {/* <Test /> */}
   </React.StrictMode>
 );
